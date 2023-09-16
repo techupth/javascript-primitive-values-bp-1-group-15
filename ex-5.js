65b6e8834b2615a93cc7e192620c11ce12710c67
pranaiya suwanta <27mar03.jk@gmail.com>
2023-09-16 15:30:18 +0700

    complete assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalProductPrice = 27000; 
+let discountRate = 0.25; 
+let discountedAmount = totalProductPrice * discountRate;
+let finalPayment = totalProductPrice - discountedAmount; 
